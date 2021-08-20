34f697e7d4b982240e5730453507482710549332
Paul N Barnes <paulieb99@gmail.com>
2021-08-19 18:11:54 -0700

    added .env to gitignore and added requires for server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;

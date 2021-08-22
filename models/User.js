96a29cdde6b8f6ec57b887386c0c014e43873c26
Paul N Barnes <paulieb99@gmail.com>
2021-08-22 12:16:48 -0700

    added checkpassword function to user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {

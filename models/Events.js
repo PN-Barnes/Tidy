3518b1491e8a6ca9a184d7ccbb20a47b4eefbcff
Paul N Barnes <paulieb99@gmail.com>
2021-08-22 16:01:39 -0700

    finished event model

--- models/Events.js
+++ models/Events.js
@@ -3,22 +3,33 @@ const sequelize = require('../config/connection');
 
 class Event extends Model {}
 
-Event.init({
-  date: {
-    type: DataTypes.DATEONLY,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-  },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  attendees: {
-    type: DataTypes.STRING,
-    references: {
-      model: 'User',
-      key: 'username',
-      unique: false,
+Event.init(
+  {
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    attendees: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'User',
+        key: 'username',
+        unique: false,
+      },
     },
   },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'event',
+  }
+);
+
+module.exports = Event;

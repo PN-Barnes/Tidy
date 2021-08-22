448f675326e014a8b585cc4867b732fd1945748b
Paul N Barnes <paulieb99@gmail.com>
2021-08-22 16:21:31 -0700

    reworked index.js now works with seeds command

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,6 @@
 const seedUsers = require('./user-seeds');
 const seedEvents = require('./event-seeds');
 const sequelize = require('../config/connection');
-const seedEvents = require('./event-seeds');
 
 const seedDB = async () => {
   await sequelize.sync({

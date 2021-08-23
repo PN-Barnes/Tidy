c0ae75a3c846053175ce459d6cf1c91a1fd5deda
Paul N Barnes <paulieb99@gmail.com>
2021-08-22 19:49:20 -0700

    connected api to be run when querying through server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,3 +4,10 @@ const userRoutes = require('./user-routes');
 const eventRoutes = require('./event-routes');
 const messageRoutes = require('./message-routes');
 const taskRoutes = require('./task-routes');
+
+router.use('/user', userRoutes);
+router.use('/event', eventRoutes);
+router.use('/message', messageRoutes);
+router.use('/task', taskRoutes);
+
+module.exports = router;

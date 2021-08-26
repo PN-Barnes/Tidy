d479d9d06a7befafc5a806f598009e82b59c5c96
Huiran Lin <hugh80082@gmail.com>
2021-08-26 07:23:20 -0700

    Add post route for task

--- controllers/api/task-routes.js
+++ controllers/api/task-routes.js
@@ -4,15 +4,31 @@ const { withAuth } = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const dbUserData = await Task.findAll({});
-    if (!dbUserData) {
+    const dbTaskData = await Task.findAll({});
+    if (!dbTaskData) {
       res.status(400).json({ message: 'No tasks were found!' });
     } else {
-      res.status(200).json(dbUserData);
+      res.status(200).json(dbTaskData);
     }
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const dbTaskData = await Task.create({
+      content: req.body.content,
+      date: req.body.date,
+      userId: req.body.userId,
+    });
+
+    const task = dbTaskData.get({ plain: true });
+    res.status(200).json(dbTaskData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 

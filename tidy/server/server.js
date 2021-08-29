f687f7d630c88c26690866ba574feb1afec10966
Paul N Barnes <paulieb99@gmail.com>
2021-08-28 19:13:51 -0700

    reverted server file

--- tidy/server/server.js
+++ tidy/server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
+const routes = require('../tidy/src/controllers');
 //const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');

25266aac16cbacd9b08a1093bac3caf5bbdd03df
Paul N Barnes <paulieb99@gmail.com>
2021-08-28 21:28:50 -0700

    refactored connection to work with our server

--- tidy/server/config/connection.js
+++ tidy/server/config/connection.js
@@ -22,14 +22,11 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/programming-thoughts',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tidy', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;

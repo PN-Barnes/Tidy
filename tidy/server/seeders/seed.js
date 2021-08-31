a4220b61f06af81760a127888ce55ffc27532111
Paul N Barnes <paulieb99@gmail.com>
2021-08-31 14:17:23 -0700

    fixed mutation for addUser

--- tidy/server/seeders/seed.js
+++ tidy/server/seeders/seed.js
@@ -1,6 +1,7 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Task } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const taskSeeds = require('./taskSeeds.json');
 
 db.once('open', async () => {
   try {

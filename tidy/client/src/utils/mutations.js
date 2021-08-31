630f4b059a6c92fd3e03a1760da354e9d9d2fcd5
Huiran Lin <hugh80082@gmail.com>
2021-08-31 13:58:14 -0700

    Add mutations to front end mutation.js

--- tidy/client/src/utils/mutations.js
+++ tidy/client/src/utils/mutations.js
@@ -1,11 +1,75 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Task, Photo, Message, workEvent } = require('../models');
-const { signToken } = require('../utils/auth');
+import { gql } from '@apollo/client';
 
-const resolvers = {
-  Query: {
-    users: async () => {
-      return User.find();
-    },
-  },
-};
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $userName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      userName: $userName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        firstName
+        lastName
+        userName
+        email
+      }
+    }
+  }
+`;
+
+export const UPDATE_USER = gql`
+  mutation updateUser($userName: String!, $email: String!, $password: Stirng!) {
+    updateUser(userName: $userName, email: $email, password: $password) {
+      user {
+        userName
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_TASK = gql`
+  mutation addTask($content: String!, $date: String!, $userId: ID!) {
+    addTask(content: $content, date: $date, userId: $userId) {
+      task {
+        content
+        date
+        userId
+      }
+    }
+  }
+`;
+
+export const UPDATE_TASK = gql`
+  mutation updateTask($content: String!, $date: Date!, $userId: ID!) {
+    updateTask(content: $content, date: $date, userId: $userId) {
+      task {
+        content
+        date
+        userId
+      }
+    }
+  }
+`;
+
+export const ADD_PHOTO = gql`
+  mutation addPhoto($url_link: String!, $description: String!, $owner: ID) {
+    addPhoto(url_link: $url_link, description: $description, owner: $owner) {
+      photo {
+        url_link
+        description
+        owner
+      }
+    }
+  }
+`;

4d6324e39602b645c1027bb1d8b1ea1449b71c16
jamesboblak <james@skepticalrecords.com>
2021-09-01 13:28:22 -0700

    added Router to App2.js

--- tidy/client/src/App2.js
+++ tidy/client/src/App2.js
@@ -203,4 +203,32 @@ const App = () => {
   );
 };
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.10.0";
+
+// pages for this product
+import Components from "views/Components/Components.js";
+import SignUp from "pages/Signup.js";
+import Home from "pages/Home.js";
+import SignIn from "pages/Login.js/";
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/Home" component={Home} />
+      <Route path="/Signup" component={SignUp} />
+      <Route path="/Login" component={SignIn} />
+      <Route path="/" component={Components} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
+
+
 export default App;

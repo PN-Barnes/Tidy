e04bdca8e3b4f5b42757eb2fd03040787c8d79b8
Paul N Barnes <paulieb99@gmail.com>
2021-09-01 19:03:40 -0700

    added more data to login mutation within the utils folder of clietn

--- tidy/client/src/utils/mutations.js
+++ tidy/client/src/utils/mutations.js
@@ -1,11 +1,12 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN = gql`
+export const LOGIN_ME = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
         _id
+        username
       }
     }
   }

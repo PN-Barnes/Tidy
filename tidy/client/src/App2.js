bcb86a1b21cfbb555580508c30c9eec6ca594525
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-02 11:23:23 -0700

    button import

--- tidy/client/src/App2.js
+++ tidy/client/src/App2.js
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+
+import Button from '@material-ui/core/Button';
 // import CssBaseline from '@material-ui/core/CssBaseline';
 import {
   Typography,

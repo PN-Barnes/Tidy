60196c2afdcbcbf21a103cc88fe7f517ef32629a
Paul N Barnes <paulieb99@gmail.com>
2021-09-02 14:39:47 -0700

    working add with develop and landing page as default / route

--- tidy/client/src/App2.js
+++ tidy/client/src/App2.js
@@ -63,7 +63,6 @@ function App() {
               <Route exact path="/calendar" component={Calendar} />
               <Route exact path="/contacts" component={Contacts} />
               <Route exact path="/events" component={Events} />
-              <Route exact path="/landing" component={Landing} />
               <Route exact path="/photos" component={Photos} />
               <Route exact path="/profile" component={Profile} />
               <Route exact path="/team" component={Team} />
@@ -72,6 +71,7 @@ function App() {
               <Route exact path="/signup" component={SignUp} />
               <Route exact path="/tasks" component={Tasks} />
               <Route exact path="/weather" component={Weather} />
+              <Route exact path="/" component={Landing} />
               <Route component={NoMatch} />
             </Switch>
           </AccountProvider>

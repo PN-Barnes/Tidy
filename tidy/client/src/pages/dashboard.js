9b588654b0f523e2d0291a20cd66105edc801e46
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-03 10:25:21 -0700

    dashboard title add

--- tidy/client/src/pages/dashboard.js
+++ tidy/client/src/pages/dashboard.js
@@ -80,7 +80,7 @@ export default function Album() {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              
+            Dashboard
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
               Something short and leading about the collection below—its contents, the creator, etc.

7750c8fc60e0c0f72bde967791e565805522da03
Huiran Lin <hugh80082@gmail.com>
2021-09-05 15:13:39 -0700

    Add layout and styling to profile.js task.js and contact.js

--- client/src/pages/contacts.js
+++ client/src/pages/contacts.js
@@ -4,6 +4,8 @@ import { Typography } from '@material-ui/core';
 import { useQuery } from '@apollo/client';
 import { QUERY_MESSAGES, QUERY_USERS } from '../utils/queries';
 
+import ContactList from '../components/contactList';
+
 function Contacts() {
   // const { loading, data } = useQuery(QUERY_MESSAGES);
 
@@ -16,18 +18,41 @@ function Contacts() {
   const users = data?.users || [];
 
   return (
-    <div className='App'>
-      <Typography variant='h1'>Contacts</Typography>
-      <div>
-        {users.map((user) => (
-          <div>
-            <p> Username: {user.username}</p>
-            <p> First Name: {user.firstName}</p>
-            <p> Last Name: {user.lastName} </p>
-          </div>
-        ))}
+    // <div className='App'>
+    //   <Typography variant='h1'>Contacts</Typography>
+    //   <div>
+    //     {loading ? (
+    //       <div>Loading...</div>
+    //     ) : (
+    //       <ContactList
+    //         events={users}
+    //         title="Here's your current roster of users:"
+    //       />
+    //     )}
+
+    //     {/* {users.map((user) => (
+    //       <div>
+    //         <p> Username: {user.username}</p>
+    //         <p> First Name: {user.firstName}</p>
+    //         <p> Last Name: {user.lastName} </p>
+    //       </div>
+    //     ))} */}
+    //   </div>
+    // </div>
+    <main>
+      <div className='flex-row justify-center'>
+        <div className='col-12 col-md-10 my-3'>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ContactList
+              users={users}
+              title="Here's your current list of contacts:"
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,8 +4,43 @@ import { Typography } from '@material-ui/core';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Profile from '../components/profile';
 
 import Auth from '../utils/auth';
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 function ToDos() {
   const { username: useParam } = useParams();
@@ -22,19 +57,33 @@ function ToDos() {
   console.log('user', user);
 
   return (
-    <div className='App'>
-      <Typography variant='h1'>Profile</Typography>
-      {/* {users.map((user) => (
-        <div>
-          <p>{user.username}</p>
-          <p>{user.email}</p>
-          <p>{user.role}</p>
-          {user.events.map((event) => (
-            <p>{event._id}</p>
-          ))}
+    // <div className='App'>
+    //   <Typography variant='h1'>Profile</Typography>
+    //   {/* {users.map((user) => (
+    //     <div>
+    //       <p>{user.username}</p>
+    //       <p>{user.email}</p>
+    //       <p>{user.role}</p>
+    //       {user.events.map((event) => (
+    //         <p>{event._id}</p>
+    //       ))}
+    //     </div>
+    //   ))} */}
+    // </div>
+    <main>
+      <div className='flex-row justify-center'>
+        <div className='col-12 col-md-10 my-3'>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <Profile
+              user={user}
+              title="Here's your current roster of events:"
+            />
+          )}
         </div>
-      ))} */}
-    </div>
+      </div>
+    </main>
   );
 }
 

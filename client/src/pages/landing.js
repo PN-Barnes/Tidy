689b4572ecb569ffe2c5521b5b9d3830c75daab2
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-04 17:00:48 -0700

    contacts link

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -215,7 +215,17 @@ const Landing = () => {
                 </CardContent>
                 <CardActions>
                   <Button size="sm" color="primary">
-                    View
+                  <Link
+                  to="/contacts"
+                  component={RouterLink}
+                  color="textSecondary"
+                  variant="h5"
+                  onClick={() => {
+                    console.info("Home Request button hit");
+                  }}
+          >
+            <Typography variant="h6">VIEW</Typography>
+          </Link>
                   </Button>
                 </CardActions>
               </Card>

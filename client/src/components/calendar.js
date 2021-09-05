4e5c08d9b469ede8e56d77533fc7fd467612ea27
jamesboblak <james@skepticalrecords.com>
2021-09-05 00:42:19 -0700

    added styled from @emotion/styled to style calendar.js

--- client/src/components/calendar.js
+++ client/src/components/calendar.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import { formatDate } from '@fullcalendar/react';
+// allows style wrapper to be created
+import styled from '@emotion/styled';
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
+
+export const StyleWrapper = styled.div`
+  .fc {
+    background-image: 
+      radial-gradient(rgba(0, 255, 0, 0.3),
+      rgba(0, 20, 0, 0.4)),
+      url("../../logo1920_42.png");
+      background-repeat: no-repeat;
+      background-size: 100%;
+  },
+
+  .fc .fc-col-header-cell-cushion {
+    
+    padding-top: 2px;
+    padding-bottom: 2px;
+    font-size: 1.4em;
+    text-align: center;
+    border: 2px solid green;
+    border-radius: 12px;
+  }
+`
+
 export default function Calendar() {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+  return (
+
+    <StyleWrapper>
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+    </StyleWrapper>
+  );
 }

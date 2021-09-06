a18c0883dfb5ac1b17f13625a4f09f8f611740cc
jamesboblak <james@skepticalrecords.com>
2021-09-06 16:57:18 -0700

    added Team premium page

--- client/src/pages/team.js
+++ client/src/pages/team.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+// import from '@material-ui/core';
+import {
+    Typography,
+    Card,
+    CardMedia,
+
+  } from "@material-ui/core";
+
+  // import styles
+import useStyles from "./styles";
 
 function Team(){
+    const classes = useStyles();
     return(
-        <div className="App">
-            <Typography variant="h1">
-                Team
-            </Typography>
+        <div>
+                <Typography variant="h1">
+                    Team
+                </Typography>
+                <Typography variant="h5" color="Secondary">
+                    PREMIUM SERVICE
+                </Typography>
+                <Typography variant="h6">
+                upgrade to Premium Account
+                </Typography>
+            <Card className={classes.card}>
+                <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/featured/?team/320x180"
+                    title="PREMIUM SERVICE!!"
+                  />
+            </Card>
         </div>
     );
 }
